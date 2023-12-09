a485af531ebeb1a503127f028f54df12fc21dc6d
shubham070 <shubhamg070@gmail.com>
2023-12-09 12:58:17 +0530

    changes for toggle login logout and search box with console

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,19 +3,42 @@ import RestaurantCard from "../components/RestaurantCard";
 
 const Body = (res) => {
   const [cards, setCards] = useState(res.res.data.data.cards);
+  const [searchText, setSearchText] = useState("");
 
-  useEffect(fetchData(), []);
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   const fetchData = async () => {
-    const data = await fetch("");
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.5205397&lng=73.8573802&page_type=DESKTOP_WEB_LISTING"
+    );
 
     const json = await data.json();
+    console.log(
+      json.data.cards[5].card.card.gridElements.infoWithStyle.restaurants
+    );
   };
 
   return (
     <div className="body">
-      <div className="Search">Search</div>
       <div className="filter">
+        <input
+          className="search-box"
+          type="text"
+          onChange={(e) => {
+            console.log(e);
+            setSearchText(e.target.value);
+          }}
+          value={searchText}
+        />
+        <button
+          onClick={() => {
+            console.log("search clicked");
+          }}
+        >
+          Search
+        </button>
         <button
           className="filter-btn"
           onClick={() => {
@@ -28,6 +51,7 @@ const Body = (res) => {
           Top Rated Restaurants
         </button>
       </div>
+
       <div className="res-container">
         {cards.map((x) => {
           return (
